2b18681bfce0e6075a38ac5ee3a624e4436d1ee0
enforcer115 <81341939+enforcer115@users.noreply.github.com>
2023-04-11 19:50:01 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,21 +6,17 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-# Change Debian to SID Branch
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-cp sources.list /etc/apt/sources.list
-
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
-# Update packages list and update system
+echo  "Update packages list and update system"
 apt update
 apt upgrade -y
 
-# Install nala
+echo "Install nala"
 apt install nala -y
 
-# Making .config and Moving config files and background to Pictures
+echo  "Making .config and Moving config files and background to Pictures"
 cd $builddir
 mkdir -p /home/$username/.config
 mkdir -p /home/$username/.fonts
@@ -35,18 +31,20 @@ chown -R $username:$username /home/$username
 tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
 mv /home/$username/.config/sddm.conf /etc/sddm.conf
 
-# Installing sugar-candy dependencies
+echo "Installing sugar-candy dependencies"
 nala install libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
-# Installing Essential Programs 
-nala install feh bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit x11-xserver-utils unzip yad wget pulseaudio pavucontrol -y
-# Installing Other less important Programs
-nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji sddm -y
 
-# Download Nordic Theme
+echo "Installing Essential Programs" 
+nala install feh bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit x11-xserver-utils unzip yad wget pulseaudio pavucontrol -y
+
+echo "Installing Other less important Programs"
+nala install firefox microsoft-edge-stable gedit neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji sddm -y
+
+echo "Download Nordic Theme"
 cd /usr/share/themes/
 git clone https://github.com/EliverLara/Nordic.git
 
-# Installing fonts
+echo "Installing fonts"
 cd $builddir 
 nala install fonts-font-awesome
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
@@ -56,26 +54,19 @@ unzip Meslo.zip -d /home/$username/.fonts
 mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
 chown $username:$username /home/$username/.fonts/*
 
-# Reloading Font
+echo "Reloading Font"
 fc-cache -vf
 # Removing zip Files
 rm ./FiraCode.zip ./Meslo.zip
 
-# Install Nordzy cursor
+echo "Install Nordzy cursor"
 git clone https://github.com/alvatip/Nordzy-cursors
 cd Nordzy-cursors
 ./install.sh
 cd $builddir
 rm -rf Nordzy-cursors
 
-# Install brave-browser
-sudo nala install apt-transport-https curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo nala update
-sudo nala install brave-browser -y
-
-# Enable graphical login and change target from CLI to GUI
+echo "Enable graphical login and change target from CLI to GUI"
 systemctl enable sddm
 systemctl set-default graphical.target
 
